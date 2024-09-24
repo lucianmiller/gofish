dbffb07221ee1c7642e4f1ed93ad649497f48875
Lucian Miller <lucian.miller185@gmail.com>
2024-09-24 14:51:54 -0700

    Add home controller

---
+++ GoFish/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoFish.Controllers
+{
+    public class HomeController : Controller
+    {
+
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        return View();
+      }
+
+    }
+}
