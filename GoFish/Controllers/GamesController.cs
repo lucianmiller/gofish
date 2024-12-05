cdbb6c3d5b8ac07bc25f7385ca576b597c703d05
Lucian Miller <lucian.miller185@gmail.com>
2024-12-05 14:37:23 -0800

    Add GamesController

---
+++ GoFish/Controllers/GamesController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using GoFish.Models;
+
+namespace GoFish.Controllers
+{
+  public class GamesController : Controller
+  {
+    [HttpGet("/games")]
+    public ActionResult Index()
+    {
+      Game newGame = new Game();
+      return View();
+    }
+  }
+}
