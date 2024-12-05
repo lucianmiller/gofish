a28863beee6a5bd9004d153650bbffce6bc47e09
Lucian Miller <lucian.miller185@gmail.com>
2024-12-05 14:07:26 -0800

    Rename PlayerHand and remove PlayerName

--- GoFish/Models/Player.cs
+++ GoFish/Models/Player.cs
@@ -5,10 +5,7 @@ namespace GoFish.Models
 {
   public class Player
   {
-    public string Player1Name { get; set; }
-    public List<Card> Player1Hand { get; set; } = new List<Card> ();
+    public List<Card> PlayerHand { get; set; } = new List<Card> ();
     public List<Card> ComputerHand { get; set; } = new List<Card> ();
   }
-
-  
 }
